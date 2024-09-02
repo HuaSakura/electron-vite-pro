af4c65640a1958db3163da35bab52f6c20dbd6a6
SakuraHan <173314746@qq.com>
2024-09-02 17:47:13 +0800

    fix: 完善初步主题和基础样式

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,50 @@
         </div>
       </div>
     </div>
-    <router-view :key="route.fullPath"/>
+    <div class="window-content">
+      <div class="app-left">
+        <div class="app-left-content">
+          <div class="app-left-content-logo">
+            <img src="./assets/favicon.ico" alt="" @click="openOfficialWebsite">
+          </div>
+          <div class="app-left-content-button">
+            <div class="button-bottom">
+              <div class="icon">
+                <svg version="1.1" role="presentation" width="20" height="20" viewBox="0 0 24 24" class="mo-icon">
+                  <g stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+                    <line x1="14" y1="4" x2="23" y2="4"></line>
+                    <line x1="1" y1="4" x2="4" y2="4"></line>
+                    <line data-color="color-2" x1="22" y1="12" x2="23" y2="12"></line>
+                    <line data-color="color-2" x1="1" y1="12" x2="12" y2="12"></line>
+                    <line x1="14" y1="20" x2="23" y2="20"></line>
+                    <line x1="1" y1="20" x2="4" y2="20"></line>
+                    <circle cx="7" cy="4" r="3"></circle>
+                    <circle data-color="color-2" cx="15" cy="12" r="3"></circle>
+                    <circle cx="7" cy="20" r="3"></circle>
+                  </g>
+                </svg>
+              </div>
+              <div class="icon">
+                <svg version="1.1" role="presentation" width="20" height="20" viewBox="0 0 24 24" class="mo-icon">
+                  <g>
+                    <g stroke-linecap="round" stroke-linejoin="round" stroke-width="2" fill="#fff" stroke="#fff">
+                      <circle fill="none" stroke="#fff" stroke-miterlimit="10" cx="12" cy="12" r="11"></circle>
+                      <path data-color="color-2" fill="none" stroke-miterlimit="10"
+                            d="M12,15v-2 c1.609,0,3-1.391,3-3s-1.391-3-3-3c-1.194,0-2.342,0.893-2.792,1.921"></path>
+                      <circle data-color="color-2" data-stroke="none" cx="12" cy="18" r="1" stroke-linejoin="miter"
+                              stroke-linecap="square" stroke="none"></circle>
+                    </g>
+                  </g>
+                </svg>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="app-content">
+        <router-view :key="route.fullPath"/>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -32,12 +75,17 @@ import {
   FullscreenOutlined,
   MinusOutlined,
   PushpinFilled,
-  PushpinOutlined
+  PushpinOutlined,
+  QuestionCircleOutlined,
+  SettingOutlined
 } from "@ant-design/icons-vue"
+import {useColorMode} from "@vueuse/core";
 
 export default defineComponent({
   name: "App",
   components: {
+    QuestionCircleOutlined,
+    SettingOutlined,
     PushpinFilled,
     MinusOutlined,
     CloseOutlined,
@@ -47,6 +95,7 @@ export default defineComponent({
   },
   setup() {
     const route = useRoute();
+    const mode = useColorMode()
     const FullScreenType = ref(true as boolean);
     const TopType = ref(true as boolean);
     const titleBar = ref(null as any);
@@ -61,15 +110,20 @@ export default defineComponent({
       window.ipcRenderer.send('top-type', !TopType.value)
     }
 
-    function CloseWindow(){
+    function CloseWindow() {
       window.ipcRenderer.send('close-window')
     }
 
-    function minimizeWindow(){
+    function minimizeWindow() {
       window.ipcRenderer.send('minimize-window')
     }
 
+    function openOfficialWebsite() {
+      window.ipcRenderer.send('open-official-website')
+    }
+
     return {
+      mode,
       route,
       TopType,
       titleBar,
@@ -77,69 +131,9 @@ export default defineComponent({
       CloseWindow,
       TopOperation,
       FullScreenType,
-      minimizeWindow
+      minimizeWindow,
+      openOfficialWebsite
     }
   }
 })
 </script>
-
-<style scoped lang="less">
-.app {
-  width: 100vw;
-  height: 100vh;
-
-  .window-operate {
-    display: flex;
-    top: 0;
-    position: fixed;
-    width: 100vw;
-    height: 36px;
-    z-index: 5000;
-
-    .window-operate-left {
-      width: 100%;
-      -webkit-app-region: drag;
-    }
-
-    .window-operate-right {
-      display: flex;
-      width: 205px;
-
-      &:hover {
-        .window-operate-icon {
-          .anticon {
-            font-size: 14px;
-            font-weight: bold;
-            color: #eee;
-          }
-        }
-      }
-
-      .window-operate-icon {
-        width: 25%;
-        height: 32px;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-
-        .anticon {
-          font-size: 14px;
-          font-weight: bold;
-          color: #888888;
-        }
-
-        &:hover {
-          background-color: #242424;
-        }
-      }
-
-      .close {
-        &:hover {
-          background-color: #FF0000;
-        }
-      }
-    }
-  }
-}
-
-</style>
